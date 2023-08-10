1dbf2d6a7324a29df861d493b1814058ea8eb77a
Marnie-108 <marnie.007.paul@outlook.com>
2023-08-10 21:52:53 +0100

    added name checker for fun

--- src/python_sandbox/scratch.py
+++ src/python_sandbox/scratch.py
@@ -1,3 +1,8 @@
 import requests
 
 print("I love Glasgow")
+name = input("What's ur name? ")
+if name.lower() == "marnie":
+    print("Welcome to ur terminal")
+else:
+    print("What are you doing in Marnie's terminal?")
